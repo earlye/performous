4ca415aa726e526ab2e8412ee13889e76c1f7523
Early Ehlinger <earlye@gmail.com>
2017-06-09 13:41:07 -0500

    Optimize osx.mk so that the whole universe doesn't recompile each time

--- osx.mk
+++ osx.mk
@@ -12,81 +12,105 @@
 # make -f osx.mk all
 # ```
 
-all : build dependencies
-	cd build && cmake \
-		-D Crypto_INCLUDE_DIR=/usr/local/Cellar/openssl/1.0.2h_1/include/openssl/ \
-		-D Ssl_INCLUDE_DIR=/usr/local/Cellar/openssl/1.0.2h_1/include/openssl/ ..
+all : build/performous
+
+build/performous: .cmake
 	cd build && make
 
 clean:
 	rm -rf build
 
-build:
-	mkdir $@
+.cmake : .dependencies
+	mkdir -p build
+	pushd build && cmake \
+		-D Crypto_INCLUDE_DIR=/usr/local/Cellar/openssl/1.0.2h_1/include/openssl/ \
+		-D Ssl_INCLUDE_DIR=/usr/local/Cellar/openssl/1.0.2h_1/include/openssl/ .. \
+		&& popd
+	touch $@
 
-dependencies: .install-cmake .install-libsdl2 .install-help2man .install-libepoxy .install-cairo .install-librsvg .install-portaudio .install-portmidi .install-opencv .install-dylibbundler .install-libxml++ .install-jsoncpp .install-openssl .install-libressl .install-boost .install-cppnetlib
+run: all
+	build/performous
+
+.dependencies: .install-cmake .install-libsdl2 .install-help2man .install-libepoxy .install-cairo .install-librsvg .install-portaudio .install-portmidi .install-opencv .install-dylibbundler .install-libxml++ .install-jsoncpp .install-openssl .install-libressl .install-boost .install-cppnetlib
+	touch $@
 
 .install-cppnetlib:
+	echo $@
 	brew install earlye/homebrew-boneyard/cpp-netlib
 	touch $@
 
 .install-boost:
+	echo $@
 	if brew ls --versions boost > /dev/null; then echo "boost is installed" ; else brew install boost; fi
 	touch $@
 
 .install-libressl:
+	echo $@
 	brew install libressl
 	touch $@
 
 .install-openssl:
+	echo $@
 	brew install openssl
 	touch $@
 
 .install-jsoncpp:
+	echo $@
 	brew install jsoncpp
 	touch $@
 
 .install-libxml++:
+	echo $@
 	brew install libxml++
 	touch $@
 
 .install-cmake:
+	echo $@
 	brew install cmake
 	touch $@
 
 .install-libsdl2 :
+	echo $@
 	brew install sdl2
 	touch $@
 
 .install-help2man:
+	echo $@
 	brew install help2man
 	touch $@
 
 .install-libepoxy:
+	echo $@
 	brew install libepoxy
 	touch $@
 
 .install-cairo:
+	echo $@
 	brew install cairo
 	touch $@
 
 .install-librsvg:
+	echo $@
 	brew install librsvg
 	touch $@
 
 .install-portaudio:
+	echo $@
 	brew install portaudio
 	touch $@
 
 .install-portmidi:
+	echo $@
 	brew install portmidi
 	touch $@
 
 .install-opencv:
+	echo $@
 	brew tap homebrew/science
 	brew install opencv3
 	touch $@
 
 .install-dylibbundler:
+	echo $@
 	brew install dylibbundler
 	touch $@
